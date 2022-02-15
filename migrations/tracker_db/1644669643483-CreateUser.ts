bb97ea33bdb9f5756e87f0c68dfad827249f8da2
Paschal Obba <pbaba@Paschals-MacBook-Pro.local>
2022-02-15 14:47:41 +0100

    chore: add healthchecks

--- migrations/tracker_db/1644669643483-CreateUser.ts
+++ migrations/tracker_db/1644669643483-CreateUser.ts
@@ -3,6 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class CreateUser1644669643483 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // await queryRunner.createTable()
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
